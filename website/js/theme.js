5220ed9ffe2d5f46d85fbdadfdd956c972f47d70
com-in <112628581+com-in@users.noreply.github.com>
2025-05-18 21:51:17 +0800

    Delete website directory

--- website/js/theme.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme() {
-    const link = document.getElementById('theme-style');
-    if (link.getAttribute('href') === 'css/themes/default.css') {
-        link.setAttribute('href', 'css/themes/dark.css');
-    } else {
-        link.setAttribute('href', 'css/themes/default.css');
-    }
-}
